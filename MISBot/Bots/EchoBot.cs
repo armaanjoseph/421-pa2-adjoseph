053b907ae69e9b1fde4b1c056df20638f584d75b
armaanjoseph <armaanjoseph@gmail.com>
2019-12-08 12:14:40 -0600

    trying to get the bot into azure

---
+++ MISBot/Bots/EchoBot.cs
@@ -0,0 +1,64 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.6.2
+
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace MISBot.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak($"Honestly, thats a whole mood."), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    var welcomeCard = CreateAdaptiveCardAttachment();
+                    var response = MessageFactory.Attachment(welcomeCard, ssml: "Welcome to Bot Framework!");
+                    await turnContext.SendActivityAsync(response, cancellationToken);
+                    await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak($"Let's Get Started!"), cancellationToken);
+                }
+            }
+        }
+
+        private IActivity CreateActivityWithTextAndSpeak(string message)
+        {
+            var activity = MessageFactory.Text(message);
+            string speak = @"<speak version='1.0' xmlns='https://www.w3.org/2001/10/synthesis' xml:lang='en-US'>
+              <voice name='Microsoft Server Speech Text to Speech Voice (en-US, JessaRUS)'>" +
+              $"{message}" + "</voice></speak>";
+            activity.Speak = speak;
+            return activity;
+        }
+        private Attachment CreateAdaptiveCardAttachment()
+        {
+            var cardResourcePath = "MISBot.Cards.Welcome.json";
+
+            using (var stream = GetType().Assembly.GetManifestResourceStream(cardResourcePath))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var adaptiveCard = reader.ReadToEnd();
+                    return new Attachment()
+                    {
+                        ContentType = "application/vnd.microsoft.card.adaptive",
+                        Content = JsonConvert.DeserializeObject(adaptiveCard),
+                    };
+                }
+            }
+        }
+    }
+}
